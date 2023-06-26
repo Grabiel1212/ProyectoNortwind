adfb26e24e03e78045304d9e05dcea0c2735fddf
JulioCesar <juliobq1996@gmail.com>
2023-06-26 18:16:50 -0500

    cree los diseños de mi formularios

--- GUINorthwind/FrmCRUEmpleado.Designer.cs
+++ GUINorthwind/FrmCRUEmpleado.Designer.cs
@@ -28,12 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGrabarEmpleado = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboEmpleado = new System.Windows.Forms.ComboBox();
+            this.txtApellidoEmpleado = new System.Windows.Forms.TextBox();
+            this.txtNombreEmpleado = new System.Windows.Forms.TextBox();
+            this.pnlBarraTitulo = new System.Windows.Forms.Panel();
+            this.lblTituloPrincipal = new System.Windows.Forms.Label();
+            this.btnMinimizar = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Label();
+            this.txtCumpleañoEmpleado = new System.Windows.Forms.TextBox();
+            this.pnlBarraTitulo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGrabarEmpleado
+            // 
+            this.btnGrabarEmpleado.BackColor = System.Drawing.Color.IndianRed;
+            this.btnGrabarEmpleado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGrabarEmpleado.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(159)))), ((int)(((byte)(127)))));
+            this.btnGrabarEmpleado.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btnGrabarEmpleado.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(118)))), ((int)(((byte)(126)))));
+            this.btnGrabarEmpleado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrabarEmpleado.ForeColor = System.Drawing.Color.LightGray;
+            this.btnGrabarEmpleado.Location = new System.Drawing.Point(243, 329);
+            this.btnGrabarEmpleado.Name = "btnGrabarEmpleado";
+            this.btnGrabarEmpleado.Size = new System.Drawing.Size(181, 40);
+            this.btnGrabarEmpleado.TabIndex = 28;
+            this.btnGrabarEmpleado.Text = "GRABAR";
+            this.btnGrabarEmpleado.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(45, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 20);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "EMPLEADO :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(45, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "CUMPLEAÑO :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(45, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "APELLIDO :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(45, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "NOMBRE :";
+            // 
+            // cboEmpleado
+            // 
+            this.cboEmpleado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cboEmpleado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEmpleado.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.cboEmpleado.FormattingEnabled = true;
+            this.cboEmpleado.Location = new System.Drawing.Point(168, 251);
+            this.cboEmpleado.Name = "cboEmpleado";
+            this.cboEmpleado.Size = new System.Drawing.Size(256, 29);
+            this.cboEmpleado.TabIndex = 21;
+            // 
+            // txtApellidoEmpleado
+            // 
+            this.txtApellidoEmpleado.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtApellidoEmpleado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtApellidoEmpleado.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellidoEmpleado.ForeColor = System.Drawing.Color.Black;
+            this.txtApellidoEmpleado.Location = new System.Drawing.Point(168, 157);
+            this.txtApellidoEmpleado.Name = "txtApellidoEmpleado";
+            this.txtApellidoEmpleado.Size = new System.Drawing.Size(256, 20);
+            this.txtApellidoEmpleado.TabIndex = 15;
+            // 
+            // txtNombreEmpleado
+            // 
+            this.txtNombreEmpleado.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtNombreEmpleado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNombreEmpleado.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreEmpleado.ForeColor = System.Drawing.Color.Black;
+            this.txtNombreEmpleado.Location = new System.Drawing.Point(168, 108);
+            this.txtNombreEmpleado.Name = "txtNombreEmpleado";
+            this.txtNombreEmpleado.Size = new System.Drawing.Size(256, 20);
+            this.txtNombreEmpleado.TabIndex = 14;
+            // 
+            // pnlBarraTitulo
+            // 
+            this.pnlBarraTitulo.BackColor = System.Drawing.Color.Brown;
+            this.pnlBarraTitulo.Controls.Add(this.lblTituloPrincipal);
+            this.pnlBarraTitulo.Controls.Add(this.btnMinimizar);
+            this.pnlBarraTitulo.Controls.Add(this.btnCerrar);
+            this.pnlBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlBarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlBarraTitulo.Name = "pnlBarraTitulo";
+            this.pnlBarraTitulo.Size = new System.Drawing.Size(458, 29);
+            this.pnlBarraTitulo.TabIndex = 13;
+            // 
+            // lblTituloPrincipal
+            // 
+            this.lblTituloPrincipal.AutoSize = true;
+            this.lblTituloPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloPrincipal.ForeColor = System.Drawing.Color.White;
+            this.lblTituloPrincipal.Location = new System.Drawing.Point(39, 5);
+            this.lblTituloPrincipal.Name = "lblTituloPrincipal";
+            this.lblTituloPrincipal.Size = new System.Drawing.Size(101, 18);
+            this.lblTituloPrincipal.TabIndex = 4;
+            this.lblTituloPrincipal.Text = "EMPLEADOS";
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnMinimizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinimizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimizar.ForeColor = System.Drawing.Color.White;
+            this.btnMinimizar.Location = new System.Drawing.Point(409, 5);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(19, 19);
+            this.btnMinimizar.TabIndex = 3;
+            this.btnMinimizar.Text = "--";
+            this.btnMinimizar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.Color.White;
+            this.btnCerrar.Location = new System.Drawing.Point(431, 5);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(20, 19);
+            this.btnCerrar.TabIndex = 4;
+            this.btnCerrar.Text = "X";
+            this.btnCerrar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCumpleañoEmpleado
+            // 
+            this.txtCumpleañoEmpleado.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtCumpleañoEmpleado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCumpleañoEmpleado.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCumpleañoEmpleado.ForeColor = System.Drawing.Color.Black;
+            this.txtCumpleañoEmpleado.Location = new System.Drawing.Point(168, 210);
+            this.txtCumpleañoEmpleado.Name = "txtCumpleañoEmpleado";
+            this.txtCumpleañoEmpleado.Size = new System.Drawing.Size(256, 20);
+            this.txtCumpleañoEmpleado.TabIndex = 29;
+            // 
+            // FrmCRUEmpleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(458, 450);
+            this.Controls.Add(this.txtCumpleañoEmpleado);
+            this.Controls.Add(this.btnGrabarEmpleado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboEmpleado);
+            this.Controls.Add(this.txtApellidoEmpleado);
+            this.Controls.Add(this.txtNombreEmpleado);
+            this.Controls.Add(this.pnlBarraTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmCRUEmpleado";
             this.Text = "FrmCRUEmpleado";
+            this.pnlBarraTitulo.ResumeLayout(false);
+            this.pnlBarraTitulo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGrabarEmpleado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboEmpleado;
+        private System.Windows.Forms.TextBox txtApellidoEmpleado;
+        private System.Windows.Forms.TextBox txtNombreEmpleado;
+        private System.Windows.Forms.Panel pnlBarraTitulo;
+        private System.Windows.Forms.Label lblTituloPrincipal;
+        private System.Windows.Forms.Label btnMinimizar;
+        private System.Windows.Forms.Label btnCerrar;
+        private System.Windows.Forms.TextBox txtCumpleañoEmpleado;
     }
 }
--- GUINorthwind/FrmCRUProveedor.Designer.cs
+++ GUINorthwind/FrmCRUProveedor.Designer.cs
@@ -28,12 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnProovedor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboEmpleado = new System.Windows.Forms.ComboBox();
+            this.txtNombreEmpleado = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.pnlBarraTitulo = new System.Windows.Forms.Panel();
+            this.lblTituloPrincipal = new System.Windows.Forms.Label();
+            this.btnMinimizar = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Label();
+            this.pnlBarraTitulo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnProovedor
+            // 
+            this.btnProovedor.BackColor = System.Drawing.Color.IndianRed;
+            this.btnProovedor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProovedor.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(159)))), ((int)(((byte)(127)))));
+            this.btnProovedor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btnProovedor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(118)))), ((int)(((byte)(126)))));
+            this.btnProovedor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProovedor.ForeColor = System.Drawing.Color.LightGray;
+            this.btnProovedor.Location = new System.Drawing.Point(294, 214);
+            this.btnProovedor.Name = "btnProovedor";
+            this.btnProovedor.Size = new System.Drawing.Size(181, 40);
+            this.btnProovedor.TabIndex = 39;
+            this.btnProovedor.Text = "GRABAR";
+            this.btnProovedor.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(22, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "COMPAÑIA :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(22, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 20);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "NOMBRE DE LA COMPAÑIA :";
+            // 
+            // cboEmpleado
+            // 
+            this.cboEmpleado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cboEmpleado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEmpleado.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.cboEmpleado.FormattingEnabled = true;
+            this.cboEmpleado.Location = new System.Drawing.Point(145, 121);
+            this.cboEmpleado.Name = "cboEmpleado";
+            this.cboEmpleado.Size = new System.Drawing.Size(256, 29);
+            this.cboEmpleado.TabIndex = 34;
+            // 
+            // txtNombreEmpleado
+            // 
+            this.txtNombreEmpleado.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtNombreEmpleado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNombreEmpleado.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreEmpleado.ForeColor = System.Drawing.Color.Black;
+            this.txtNombreEmpleado.Location = new System.Drawing.Point(254, 85);
+            this.txtNombreEmpleado.Name = "txtNombreEmpleado";
+            this.txtNombreEmpleado.Size = new System.Drawing.Size(238, 20);
+            this.txtNombreEmpleado.TabIndex = 32;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUser.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.ForeColor = System.Drawing.Color.Black;
+            this.txtUser.Location = new System.Drawing.Point(137, 130);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(408, 20);
+            this.txtUser.TabIndex = 30;
+            // 
+            // pnlBarraTitulo
+            // 
+            this.pnlBarraTitulo.BackColor = System.Drawing.Color.Brown;
+            this.pnlBarraTitulo.Controls.Add(this.lblTituloPrincipal);
+            this.pnlBarraTitulo.Controls.Add(this.btnMinimizar);
+            this.pnlBarraTitulo.Controls.Add(this.btnCerrar);
+            this.pnlBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlBarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlBarraTitulo.Name = "pnlBarraTitulo";
+            this.pnlBarraTitulo.Size = new System.Drawing.Size(522, 29);
+            this.pnlBarraTitulo.TabIndex = 31;
+            // 
+            // lblTituloPrincipal
+            // 
+            this.lblTituloPrincipal.AutoSize = true;
+            this.lblTituloPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloPrincipal.ForeColor = System.Drawing.Color.White;
+            this.lblTituloPrincipal.Location = new System.Drawing.Point(39, 5);
+            this.lblTituloPrincipal.Name = "lblTituloPrincipal";
+            this.lblTituloPrincipal.Size = new System.Drawing.Size(106, 18);
+            this.lblTituloPrincipal.TabIndex = 4;
+            this.lblTituloPrincipal.Text = "PROOVEDOR";
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnMinimizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinimizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimizar.ForeColor = System.Drawing.Color.White;
+            this.btnMinimizar.Location = new System.Drawing.Point(473, 5);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(19, 19);
+            this.btnMinimizar.TabIndex = 3;
+            this.btnMinimizar.Text = "--";
+            this.btnMinimizar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.Color.White;
+            this.btnCerrar.Location = new System.Drawing.Point(495, 5);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(20, 19);
+            this.btnCerrar.TabIndex = 4;
+            this.btnCerrar.Text = "X";
+            this.btnCerrar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmCRUProveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(522, 278);
+            this.Controls.Add(this.btnProovedor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboEmpleado);
+            this.Controls.Add(this.txtNombreEmpleado);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.pnlBarraTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmCRUProveedor";
             this.Text = "FrmCRUProveedor";
+            this.pnlBarraTitulo.ResumeLayout(false);
+            this.pnlBarraTitulo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnProovedor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboEmpleado;
+        private System.Windows.Forms.TextBox txtNombreEmpleado;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Panel pnlBarraTitulo;
+        private System.Windows.Forms.Label lblTituloPrincipal;
+        private System.Windows.Forms.Label btnMinimizar;
+        private System.Windows.Forms.Label btnCerrar;
     }
 }
